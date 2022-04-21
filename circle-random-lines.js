b47cb1105b1a21a5de5c13eedeb32c0a3ffd340f
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-04-21 20:40:13 +0300

    responsive radius

--- circle-random-lines.js
+++ circle-random-lines.js
@@ -2,21 +2,26 @@ let x, y;
 let newX, newY;
 
 function setup() {
-	createCanvas(windowWidth - (0.4*windowWidth/100), windowHeight- (0.4*windowHeight/100));
-	background(0);
-  x = width/2;
-  y = height/2;
+  createCanvas(
+    windowWidth - (0.4 * windowWidth) / 100,
+    windowHeight - (0.4 * windowHeight) / 100
+  );
+  background(0);
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
   strokeWeight(0.1);
   stroke(255);
-	
-  let radius = random(windowHeight/2.5);
+
+  let radius = random(
+    windowHeight > windowWidth ? windowWidth / 2.5 : windowHeight / 2.5
+  );
   let a = random(TWO_PI);
-	
-  newX = width/2 + cos(a)*  radius;
-  newY = height/2 + sin(a) * radius;
+
+  newX = width / 2 + cos(a) * radius;
+  newY = height / 2 + sin(a) * radius;
   line(x, y, newX, newY);
   x = newX;
   y = newY;
